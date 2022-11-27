9228749d8e5a6d76ef4cbe02056b3c6f10794992
Aryan Gupta <72256631+aryangupta24@users.noreply.github.com>
2022-11-27 14:01:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,27 @@
-const express = require('express'),
-http = require('http'),
-app = express(),
-server = http.createServer(app),
-io = require('socket.io').listen(server);
-app.get('/', (req, res) => {
+const express = require('express'); //requires express module
+const socket = require('socket.io'); //requires socket.io module
+const fs = require('fs');
+const app = express();
+var PORT = process.env.PORT || 3000;
+const server = app.listen(PORT); //tells to host server on localhost:3000
 
-res.send('Chat Server is running on port 3000')
-});
+
+//Playing variables:
+app.use(express.static('public')); //show static files in 'public' directory
+console.log('Server is running');
+const io = socket(server);
+
+var count = 0;
+
+
+//Socket.io Connection------------------
 io.on('connection', (socket) => {
 
-console.log('user connected')
+    console.log("New socket connection: " + socket.id)
 
-socket.on('join', function(userNickname) {
-
-        console.log(userNickname +" : has joined the chat "  );
-
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+    socket.on('counter', () => {
+        count++;
+        console.log(count)
+        io.emit('counter', count);
     })
-
-
-socket.on('messagedetection', (senderNickname,messageContent) => {
-
-       //log the message in console 
-
-       console.log(senderNickname+" : " +messageContent)
-
-      //create a message object 
-
-      let  message = {"message":messageContent, "senderNickname":senderNickname}
-
-       // send the message to all users including the sender  using io.emit() 
-
-      io.emit('message', message )
-
-      })
-
-socket.on('disconnect', function() {
-
-        console.log(userNickname +' has left ')
-
-        socket.broadcast.emit( "userdisconnect" ,' user has left')
-
-
-
-
-    })
-
-
-
-
-})
-
-
-
-
-
-
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000')
-
 })
