9dac0b532014e606bbe905bb78add2825ee42b9b
Aryan Gupta <72256631+aryangupta24@users.noreply.github.com>
2022-11-27 13:57:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,106 +1,63 @@
-console.log("Starting Socket Server For Niotron's Socket.io Compoennt...");
+const express = require('express'),
+http = require('http'),
+app = express(),
+server = http.createServer(app),
+io = require('socket.io').listen(server);
+app.get('/', (req, res) => {
 
-/* 1st npm init -y,
- |
- +- Required Modules
-    |
-    +- 1. express ['npm install express']
-    +- 2. http ['npm unstall http']
-    +- 3. socket.io ['npm install socket.io'] 
-*/
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const socketio = require("socket.io")(http); 
-const bodyParser = require('body-parser');
+res.send('Chat Server is running on port 3000')
+});
+io.on('connection', (socket) => {
 
-app.use(bodyParser.urlencoded({ extended: true }));
+console.log('user connected')
 
-app.use(bodyParser.json());
+socket.on('join', function(userNickname) {
+
+        console.log(userNickname +" : has joined the chat "  );
+
+        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
+    })
 
 
-const port = process.env.PORT || 3000;
+socket.on('messagedetection', (senderNickname,messageContent) => {
 
-var server = app.listen(port,()=>{
-    console.log('Server is running on port number 3000')
+       //log the message in console 
+
+       console.log(senderNickname+" : " +messageContent)
+
+      //create a message object 
+
+      let  message = {"message":messageContent, "senderNickname":senderNickname}
+
+       // send the message to all users including the sender  using io.emit() 
+
+      io.emit('message', message )
+
+      })
+
+socket.on('disconnect', function() {
+
+        console.log(userNickname +' has left ')
+
+        socket.broadcast.emit( "userdisconnect" ,' user has left')
+
+
+
+
+    })
+
+
+
+
 })
 
 
-//Chat Server
 
-var io = socketio.listen(server)
 
-io.on('connection',function(socket) {
 
-    //The moment one of your client connected to socket.io server it will obtain socket id
-    //Let's print this out.
-    console.log(`Connection : SocketId = ${socket.id}`)
-    //Since we are going to use userName through whole socket connection, Let's make it global.   
-    var userName = '';
-    
-    socket.on('subscribe', function(data) {
-        console.log('subscribe trigged')
-        const room_data = JSON.parse(data)
-        userName = room_data.userName;
-        const roomName = room_data.roomName;
-    
-        socket.join(`${roomName}`)
-        console.log(`Username : ${userName} joined Room Name : ${roomName}`)
-        
-       
-        // Let the other user get notification that user got into the room;
-        // It can be use to indicate that person has read the messages. (Like turns "unread" into "read")
 
-        //TODO: need to chose
-        //io.to : User who has joined can get a event;
-        //socket.broadcast.to : all the users except the user who has joined will get the message
-        // socket.broadcast.to(`${roomName}`).emit('newUserToChatRoom',userName);
-        io.to(`${roomName}`).emit('newUserToChatRoom',userName);
+server.listen(3000,()=>{
 
-    })
+console.log('Node app is running on port 3000')
 
-    socket.on('unsubscribe',function(data) {
-        console.log('unsubscribe trigged')
-        const room_data = JSON.parse(data)
-        const userName = room_data.userName;
-        const roomName = room_data.roomName;
-    
-        console.log(`Username : ${userName} leaved Room Name : ${roomName}`)
-        socket.broadcast.to(`${roomName}`).emit('userLeftChatRoom',userName)
-        socket.leave(`${roomName}`)
-    })
-
-    socket.on('newMessage',function(data) {
-        console.log('newMessage triggered')
-
-        const messageData = JSON.parse(data)
-        const messageContent = messageData.messageContent
-        const roomName = messageData.roomName
-
-        console.log(`[Room Number ${roomName}] ${userName} : ${messageContent}`)
-        
-        // Just pass the data that has been passed from the writer socket
-        const chatData = {
-            userName : userName,
-            messageContent : messageContent,
-            roomName : roomName
-        }
-        socket.broadcast.to(`${roomName}`).emit('updateChat',JSON.stringify(chatData)) // Need to be parsed into Kotlin object in Kotlin
-    })
-
-    //If you want to add typing function you can make it like this.
-    
-    // socket.on('typing',function(roomNumber){ //Only roomNumber is needed here
-    //     console.log('typing triggered')
-    //     socket.broadcast.to(`${roomNumber}`).emit('typing')
-    // })
-
-    // socket.on('stopTyping',function(roomNumber){ //Only roomNumber is needed here
-    //     console.log('stopTyping triggered')
-    //     socket.broadcast.to(`${roomNumber}`).emit('stopTyping')
-    // })
-
-    socket.on('disconnect', function () {
-        console.log("One of sockets disconnected from our server.")
-    });
 })
